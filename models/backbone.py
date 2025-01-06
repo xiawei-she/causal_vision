19066b485395fca4f95227f905125a8bc7c5233c
xiawei-she <shexiawei@gmail.com>
2025-01-06 14:41:39 -0500

    add the parameter backbone_only

--- models/backbone.py
+++ models/backbone.py
@@ -5,23 +5,28 @@ from models import vit
 
 
 class Backbone(nn.Module):
-    def __init__(self,backbone_type):
+    def __init__(self,backbone_type,backbone_only=True):
         super(Backbone, self).__init__()
         self.sigmoid = nn.Sigmoid()
+        self.backbone_only = backbone_only
 
         self.backbone_type = backbone_type
         assert backbone_type in {'cnn','vit'}, 'wrong backbone type'
 
         if self.backbone_type == 'cnn':
             self.model = models.resnet34(weights=ResNet34_Weights)
-            self.model.fc = nn.Linear(512, 1)
+            if backbone_only:
+                self.model.fc = nn.Linear(1000, 1)
         elif self.backbone_type == 'vit':
             self.model = models.vit_l_16(weights=ViT_L_16_Weights)
-            self.model.head = nn.Linear(512, 1)
+            if backbone_only:
+                self.model.fc = nn.Linear(1000, 1)
 
     def forward(self, x):
-        output = self.sigmoid(self.model(x))
-        return output
+        if self.backbone_only:
+            return self.model(x)
+        else:
+            return self.sigmoid(self.model(x))
 
 
 if __name__ == '__main__':
